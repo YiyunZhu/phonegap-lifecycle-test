83159bcb9129cf1358434803a1751301669277d0
YiyunZhu <zhuy3_15@uni.worc.ac.uk>
2016-10-20 15:00:38 +0100

    change to onLoad()

--- www/js/index.js
+++ www/js/index.js
@@ -7,23 +7,18 @@ var launched_count = 0;
 		console.log("device ready");
         
    //     $('#button1').click(myFunction);
-        
-    }
-	
-	function updateDisplay() {
-		$("#launched").text("Application launched: " + launched_count);
-		$("#resumed").text("Application paused: " + paused_count);
-		$("#paused").text("Application resumed: " + resumed_count);
-        
         window.localStorage.setItem("pen","black");
         window.localStorage.setItem("notebook","large");
         
       //  var key = "pen";
       //  var value = window.localStorage.getItem(key);
         alert(window.localStorage.length);
-            
-        
-        
+    }
+	
+	function updateDisplay() {
+		$("#launched").text("Application launched: " + launched_count);
+		$("#resumed").text("Application paused: " + paused_count);
+		$("#paused").text("Application resumed: " + resumed_count);        
 	}
 
 
